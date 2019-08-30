a81db2acdaaae7555bc5c03d33abbad0b7695432
jimz <luisjc140992@gmail.com>
2019-08-30 14:41:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Menu, Row, Col } from 'antd';
+import './assets/css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Item } = Menu;
+
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      current: 'mail',
+    };
+    
+  }
+
+  handleClick = e => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  };
+
+
+
+  render() {
+    return (
+        <div>
+          <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+            <Item>
+              <h2>Home</h2>
+            </Item>
+            <Item className="btn-history">
+              <h3>History</h3>
+            </Item>
+          </Menu>
+          <Row>
+            <div className="movies-container">
+              <Col span={4}>
+                <div className="cart-container">
+                  <img src="https://thetvtraveler.com/wp-content/uploads/2019/03/endgame.jpg" />
+                  <p>Avengers</p>
+                </div>
+              </Col>
+              <Col span={4}>
+                <div className="cart-container">
+                  <img src="https://thetvtraveler.com/wp-content/uploads/2019/03/endgame.jpg" />
+                  <p>Avengers</p>
+                </div>
+              </Col>
+              <Col span={4}>
+                <div className="cart-container">
+                  <img src="https://thetvtraveler.com/wp-content/uploads/2019/03/endgame.jpg" />
+                  <p>Avengers</p>
+                </div>
+              </Col>
+              <Col span={4}>
+                <div className="cart-container">
+                  <img src="https://thetvtraveler.com/wp-content/uploads/2019/03/endgame.jpg" />
+                  <p>Avengers</p>
+                </div>
+              </Col>
+              <Col span={4}>
+                <div className="cart-container">
+                  <img src="https://thetvtraveler.com/wp-content/uploads/2019/03/endgame.jpg" />
+                  <p>Avengers</p>
+                </div>
+              </Col>
+              <Col span={4}>
+                <div className="cart-container">
+                  <img src="https://thetvtraveler.com/wp-content/uploads/2019/03/endgame.jpg" />
+                  <p>Avengers</p>
+                </div>
+              </Col>
+              <Col span={4}>
+                <div className="cart-container">
+                  <img src="https://thetvtraveler.com/wp-content/uploads/2019/03/endgame.jpg" />
+                  <p>Avengers</p>
+                </div>
+              </Col>
+              <Col span={4}>
+                <div className="cart-container">
+                  <img src="https://thetvtraveler.com/wp-content/uploads/2019/03/endgame.jpg" />
+                  <p>Avengers</p>
+                </div>
+              </Col>
+              <Col span={4}>
+                <div className="cart-container">
+                  <img src="https://thetvtraveler.com/wp-content/uploads/2019/03/endgame.jpg" />
+                  <p>Avengers</p>
+                </div>
+              </Col>
+            </div>
+          </Row>
+          
+
+        </div>
+    )
+  }
 }
-
-export default App;

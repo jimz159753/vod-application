52837bdaa6f0ce50058296abfedb28ce0582a96b
jimz <luisjc140992@gmail.com>
2019-09-02 12:50:45 -0500

    adding heroku

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
-
-import { Menu, Row, Card, Carousel } from 'antd';
+import { Menu, Row, Card, Carousel, List } from 'antd';
 import Modal from 'react-modal';
 import ReactPlayer from 'react-player';
+import ls from 'local-storage';
+import { wobble } from 'react-animations';
+import styled, { keyframes } from 'styled-components';
+import Hotkeys from 'react-hot-keys';
 import 'antd/dist/antd.css';
 import './assets/css/App.css';
 
 const { Item } = Menu;
 const { Meta } = Card;
 
+const bounceAnimation = keyframes`${wobble}`;
+const BouncyDiv = styled.div`
+  animation: 1s ${bounceAnimation};
+`;
 
-export default class App extends Component {
+class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       movie: {},
       data: {},
-      current: 'mail',
+      current: 'item_0',
       modalIsOpen: false,
+      history: [],
     };
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
@@ -25,13 +33,19 @@ export default class App extends Component {
   }
 
   componentDidMount() {
+    const { history } = this.state
     fetch('https://demo2697834.mockable.io/movies')
     .then(response => response.json())
     .then(data => this.setState({data}));
+    ls('history', history);
   }
 
   openModal(movie) {
-    this.setState({modalIsOpen: true, movie});
+    const { history } = this.state;
+    const currentHistory = history;
+    currentHistory.push(movie.title)
+    this.setState({modalIsOpen: true, movie, history: currentHistory});
+    ls('history', history);
   }
 
   closeModal() {
@@ -39,16 +53,22 @@ export default class App extends Component {
   }
 
   handleClick(e) {
-    console.log('click ', e);
     this.setState({
       current: e.key,
     });
   };
 
-
+  onKeyRight(keyName, e, handle) {
+    console.log("test:onKeyUp", keyName, e, handle)
+    
+  }
+  onKeyDown(keyName, e, handle) {
+    console.log("test:onKeyDown", keyName, e, handle)
+    
+  }
 
   render() {
-    const { data, movie } = this.state;
+    const { data, movie, current } = this.state;
     const customStyles = {
             content: {
             width: '80%',
@@ -59,62 +79,81 @@ export default class App extends Component {
             } 
           }
     
-    console.log(data)
-    console.log('movie',movie)
+    
+    
+    console.log('HISTORY',ls('history'))
+    const history = ls('history')
     return (
+      <Hotkeys
+      keyName="command+left,command+right" 
+      onKeyDown={this.onKeyDown.bind(this)}
+      nKeyRight={this.onKeyRight.bind(this)}>
         <div>
           <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
             <Item>
-              <h1>VOD APPLICATION</h1>
+              <BouncyDiv>
+                <h1>VOD APPLICATION</h1>
+              </BouncyDiv>
             </Item>
             <Item className="btn-history">
               <h2>History</h2>
             </Item>
           </Menu>
-          <Row>
-            <Carousel effect='scrollx'>
-              <div>
-                {
-                  data.entries?
-                    data.entries.map(mov =>
-                      (<Card
-                          key={mov.id}
-                          onClick={()=>this.openModal(mov)}
-                          hoverable
-                          style={{ height: '100%',width: '250px' }}
-                          cover={<img 
-                                    alt="example" 
-                                    src={mov.images[0].url || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFpKVUcVn3fiOjuwf5hLu56tIZtSE5HRjw7wQOEmFpWkbd1kWd'} />}
-                        >
-                          <Meta title={mov.title}/>
-                        </Card>)
-                    )
-                  :
-                    null
-                }
-              </div>
-            </Carousel>
-            <Modal
-              style={customStyles}
-              isOpen={this.state.modalIsOpen}
-              onRequestClose={this.closeModal}
-              contentLabel="modal-cart-container"
-              ariaHideApp={false}
-            >
-              <button onClick={this.closeModal}>close</button>
-              <p>modal</p>
-              <div className="video-container">
-                <ReactPlayer 
-                    url={movie.contents?movie.contents[0].url:''} 
-                    playing 
-                    controls 
-                    onEnded={this.closeModal}/>
-              </div>
-            </Modal>
-          </Row>
-          
-
+          { current === 'item_0' ?
+              <Row>
+                <Carousel effect='scrollx'>
+                  <div>
+                    {
+                      data.entries?
+                        data.entries.map(mov =>
+                          (<Card
+                              key={mov.id}
+                              onClick={()=>this.openModal(mov)}
+                              hoverable
+                              style={{ height: '100%',width: '250px' }}
+                              cover={<img 
+                                        alt="example" 
+                                        src={mov.images[0].url || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFpKVUcVn3fiOjuwf5hLu56tIZtSE5HRjw7wQOEmFpWkbd1kWd'} />}
+                            >
+                              <Meta title={mov.title}/>
+                            </Card>)
+                        )
+                      :
+                        null
+                    }
+                  </div>
+                </Carousel>
+                <Modal
+                  style={customStyles}
+                  isOpen={this.state.modalIsOpen}
+                  onRequestClose={this.closeModal}
+                  contentLabel="modal-cart-container"
+                  ariaHideApp={false}
+                >
+                  <button className="btn-modal" onClick={this.closeModal}>close</button>
+                  <div className="video-container">
+                    <ReactPlayer
+                        width='80%'
+                        height='80%'
+                        url={movie.contents?movie.contents[0].url:''} 
+                        playing 
+                        controls 
+                        onEnded={this.closeModal}/>
+                  </div>
+                </Modal>
+              </Row>
+            :
+              <List
+                size="large"
+                bordered
+                dataSource={history}
+                renderItem={item => <List.Item>{item}</List.Item>}
+              />
+        }
         </div>
+        </Hotkeys>
     )
   }
 }
+
+export default App;
